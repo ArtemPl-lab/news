873271de74ee12e594780e8f37c14f1ab3f8ea23
artem <platonovartem2005@outlook.com>
2020-11-22 22:57:03 +0300

    parser

--- includes/sitemap_check.js
+++ includes/sitemap_check.js
@@ -3,46 +3,18 @@ const News = require('../models/News')
 const axios = require('axios')
 const {cyrillicToTranslit} = require('cyrillic-to-translit-js')
 
-function sitemapCheck(sitemapLink) {
+async function sitemapCheck(sitemapLink) {
     try {
-        const resource = Resource.find({sitemapLink: sitemapLink})
-
-        let sitemapLinks = await axios.get(sitemapLink)        
-
-        sitemapLinks = sitemapLinks.matchAll(/<loc>(.*?)<\/loc>/g)
-
-        sitemapLinks = Array.from(sitemapLinks);
-
-        for (let i = 0; i < length(sitemapLinks); i++) {
-            if (!News.find({ newsUrl: sitemapLinks[i] })) {
-                
-                let siteHtml = axios.get(sitemapLink)
-                
-                const newsTitle = siteHtml.match(/<title>(.*?)<\/title>/)
-                const newsContent = siteHtml.match(/<body>(.*?)<\/body>/)
-                const newsUrl = cyrillicToTranslit().transform(newsTitle.toLowerCase(),"-") 
-                const now = new Date
-
-                const news = new News({
-                    _id: new mongoose.Types.ObjectId(),
-                    newsTitle: newsTitle[1],
-                    newsContent: newsContent[1],
-                    newsUrl,
-                    now,
-                    resource: resource._id
-                })
-
-                await news.save()
-            }
-        }
-
-        // const newsObj = await News.find({resource: resource._id})
-
-        // for (let news in newsObj) {
-
-        // }
-
+        const resource = Resource.find({sitemapLink: sitemapLink});
+        let { data } = await axios.get(sitemapLink);
+        let sitemapLinks = [...data.matchAll(/<loc>(.*?)<\/loc>/g)];
+        sitemapLinks.map(async link => {
+            let { data: siteHtml } = await axios.get(link[1]);
+            console.log(siteHtml);
+        });
     } catch (e) {
         console.log(e);
     }
 }
+
+sitemapCheck("https://www.forbes.ru/sitemap.xml?page=154");
