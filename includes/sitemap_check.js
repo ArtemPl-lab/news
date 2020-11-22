401e23392dc74a19d8fa54a84f901a94480bbdb9
Илья <foreth35@gmail.com>
2020-11-22 21:00:45 +0300

    SitemapCheck function updated

--- includes/sitemap_check.js
+++ includes/sitemap_check.js
@@ -1,22 +1,15 @@
 const Resource = require('../models/Resource')
 const News = require('../models/News')
 const axios = require('axios')
+const {cyrillicToTranslit} = require('cyrillic-to-translit-js')
 
 function sitemapCheck(sitemapLink) {
     try {
-        const resource = await Resource.find({sitemapLink: sitemapLink})
+        const resource = Resource.find({sitemapLink: sitemapLink})
 
-        let sitemapLinks
+        let sitemapLinks = await axios.get(sitemapLink)        
 
-        axios.get(sitemapLink)
-            .then(response => {
-                sitemapLinks = response.data
-            })
-            .catch(error => {
-                console.log(error)
-            })
-
-        sitemapLinks = str.matchAll(/<loc>(.*?)<\/loc>/g)
+        sitemapLinks = sitemapLinks.matchAll(/<loc>(.*?)<\/loc>/g)
 
         sitemapLinks = Array.from(sitemapLinks);
 
@@ -31,9 +24,9 @@ function sitemapCheck(sitemapLink) {
                     console.log(error)
                 })
 
-                const newsTitle = str.match(/<title>(.*?)<\/title>/)
-                const newsContent = str.match(/<body>(.*?)<\/body>/)
-                const newsUrl = sitemapLinks[i]
+                const newsTitle = siteHtml.match(/<title>(.*?)<\/title>/)
+                const newsContent = siteHtml.match(/<body>(.*?)<\/body>/)
+                const newsUrl = cyrillicToTranslit().transform(newsTitle.toLowerCase(),"-") 
                 const now = new Date
 
                 const news = new News({
