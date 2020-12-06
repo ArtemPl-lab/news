dac67fb5ded95e004378cc63d521f56471894b2b
artem <platonovartem2005@outlook.com>
2020-12-06 10:06:50 +0300

    rebuild

--- pages/search/[query].js
+++ pages/search/[query].js
@@ -1,34 +1,44 @@
-import { Container } from '@material-ui/core';
+import { Container, Grid, Typography } from "@material-ui/core";
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import NewsCard from "../../components/News/NewsCard";
 const PostPage  = () => {
     const router = useRouter();
     console.log(router.query.query);
     const [posts, setPosts] = useState();
     useEffect(async () => {
-        const response = await fetch('/api/news/search', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json;charset=utf-8'
-            },
-            body: JSON.stringify({
-                newsTitle: router.query.query
-            })
-        });
-        console.log(await response.json());
+        if(router.query.query){
+            const response = await fetch('/api/news/search', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json;charset=utf-8'
+                },
+                body: JSON.stringify({
+                    newsTitle: router.query.query
+                })
+            });
+            setPosts(await response.json());
+        }
     },[router.query.query]);
     if(!posts) return <CircularProgress />
     return (
-        <Container>
-            {/* <Grid spacing={3} container>
-            {postsStore.posts.map(post => (
-                <Grid item xs={12} md={3} sm={6} key={post.url}>
-                <NewsCard post={post}/>
+        <>
+            <Container>
+                <Typography>
+                    <h1>Результаты поиска по запросу: {router.query.query}</h1>
+                </Typography> 
+            </Container>
+            <Container>
+                <Grid spacing={3} container>
+                {posts.map(post => (
+                    <Grid item xs={12} md={3} sm={6} key={post.url}>
+                    <NewsCard post={post}/>
+                    </Grid>
+                ))}
                 </Grid>
-            ))}
-            </Grid> */}
-        </Container>
+            </Container>
+        </>
     );
 }
 
